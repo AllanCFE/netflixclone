8fb6a77b3fa4be929e43f5331cda5ef849d495a5
HydroBR <allan.echeverria@gmail.com>
2021-01-09 01:10:01 -0300

    Getting movies from database

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 let webapp = () => {
   return (
-    <div>Hello World!</div>
+  <div>Hello World!</div>
   );
 }
 
---
+++ src/tmdb.js
@@ -0,0 +1,62 @@
+import api_keys from './private/api_key.json';
+const api_base = 'https://api.themoviedb.org/3/';
+const TMDB = api_keys.tmdb;
+
+/*
+- Netflix Originals
+- Trending
+- Top rated
+- Action
+- Horror
+- Documentary
+- Drama
+*/
+
+const basic_fetch = async (endpoint) => {
+    const req = await fetch(`${api_base}${endpoint}`);
+    const json = await req.json();
+    return json;
+};
+
+export default {
+    getHomeList: async () => {
+        return [
+            {
+                slug: 'originals',
+                title: 'Netflix Originals',
+                items: await basic_fetch(`/discover/tv/?with_network=213&api_key=${TMDB}`)
+            },
+            {
+                slug: 'trending',
+                title: 'For you',
+                items: await basic_fetch(`/trending/all/week?api_key=${TMDB}`)
+
+            },
+            {
+                slug: 'toprated',
+                title: 'Top rated',
+                items: await basic_fetch(`/movie/top_rated?api_key=${TMDB}`)
+            },
+            {
+                slug: 'action',
+                title: 'Action',
+                items: await basic_fetch(`/discover/movie?with_genres=28&api_key=${TMDB}`)
+            },
+            {
+                slug: 'horror',
+                title: 'Horror',
+                items: await basic_fetch(`/discover/movie?with_genres=2&api_key=${TMDB}`)
+            },
+            {
+                slug: 'documentary',
+                title: 'Documentary',
+                items: await basic_fetch(`/discover/movie?with_genres=99&api_key=${TMDB}`)
+            },
+            {
+                slug: 'drama',
+                title: 'Drama',
+                items: await basic_fetch(`/discover/movie?with_genres=18&api_key=${TMDB}`)
+            }
+        ];
+    }
+}
