00236991108554ab838695f26b3a9da495cdb2d8
HydroBR <allan.echeverria@gmail.com>
2021-01-10 02:07:59 -0300

    Changes in CSS (overflow:hidden)

--- src/components/Spotlight/index.js
+++ src/components/Spotlight/index.js
@@ -9,6 +9,12 @@ export default ({ item }) => {
         genres.push(item.genres[i].name);
     }
 
+    let description = item.overview;
+
+    if(description.length > 200) {
+        description = description.substring(0,200)+'[...]';
+    }
+
     return (
         <section className="spotlight" style={{ 
             backgroundSize: 'cover',
@@ -23,7 +29,7 @@ export default ({ item }) => {
                         <div className="spotlight--year">{firstDate.getFullYear()}</div>
                         <div className="spotlight--seasons">{item.number_of_seasons} season{item.number_of_seasons !== 1 ? 's' : ''}</div>
                     </div>
-                <div className="spotlight--description">{item.overview}</div>
+                <div className="spotlight--description">{description}</div>
                 <div className="spotlight--buttons">
                     <a href={`/watch/${item.id}`} className="spotlight--watchbtn"> Assistir</a>
                     <a href={`/list/add/${item.id}`} className="spotlight--mylistbtn">+Minha Lista</a>
